105acae3d614b654143bbe257a8c32fafcb3403c
deepak <deepak.trahan854@gmail.com>
2024-11-25 18:19:38 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PRODUCTPAGE/', // Replace 'PRODUCTPAGE' with your repository name
+  base: '/Productpage/', // Replace 'PRODUCTPAGE' with your repository name
 })
